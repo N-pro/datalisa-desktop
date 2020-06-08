fee458304cfcb2da32120f2b5def29791bfaf8bd
like_a_doge <like_a_doge@outlook.com>
2020-06-09 00:00:48 +0800

    Update index.page.vue

--- src/modules/transfer/view/index.page.vue
+++ src/modules/transfer/view/index.page.vue
@@ -7,9 +7,9 @@
     next-icon="mdi-arrow-right-bold-box-outline"
     prev-icon="mdi-arrow-left-bold-box-outline"
   >
-    <v-tab :href="`#tab-0`" :style="{marginLeft:'56px'}">新建项目</v-tab>
+    <v-tab :href="`#tab-0`" :style="{marginLeft:'56px'}"><v-icon left small>mdi-calendar-text</v-icon>新建项目</v-tab>
 
-    <v-tab :href="`#tab-1`">历史记录</v-tab>
+    <v-tab :href="`#tab-1`"><v-icon left small>mdi-format-list-numbered</v-icon>历史记录</v-tab>
 
     <v-tab-item :value="'tab-0'">
       <plan/>
